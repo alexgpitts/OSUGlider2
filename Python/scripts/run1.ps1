514cba2e80b3f60d1ab2a275fd1947b7d4898ebd
Alex Pitts <46640378+alexgpitts@users.noreply.github.com>
2022-05-09 09:28:47 -0700

    Edited scripts

--- Python/scripts/run1.ps1
+++ Python/scripts/run1.ps1
@@ -2,5 +2,3 @@
 python .\csv_extractor.py --cdip .\ncFiles\067.20201225_1200.20201227_0400.nc
 python .\preprocessor.py --group=Meta --group=Wave --group=XYZ ".\ncFiles\067.20201225_1200.20201227_0400.nc"
 python .\Driver.py ".\ncFiles\067.20201225_1200.20201227_0400_output.nc"
-python .\tests\test_non_directional_values.py --graph ".\ncFiles\067.20201225_1200.20201227_0400_output_processed.nc" ".\ncFiles\067.20201225_1200.20201227_0400.nc"
-python .\tests\test_directional_values.py --graph ".\ncFiles\067.20201225_1200.20201227_0400_output_processed.nc" ".\ncFiles\067.20201225_1200.20201227_0400.nc"
