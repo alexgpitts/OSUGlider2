8685e20c84a7829cebdba29aad6013b6fb889576
Alex Pitts <46640378+alexgpitts@users.noreply.github.com>
2022-05-12 19:32:25 -0700

    Update run.ps1

--- Python/scripts/run.ps1
+++ Python/scripts/run.ps1
@@ -1,5 +1,8 @@
 # PI.ps1
 git clean -fx
-$file=".\ncFiles\067.20201225_1200.20201227_0400.nc"
-python .\csv_extractor.py --cdip $file
-git clean -fx
+$file=".\ncFiles\067.20201225_1200.20201227_0400"
+python .\csv_extractor.py --cdip $file".nc"
+python .\preprocessor.py --group=Meta --group=Wave --group=XYZ $file".nc"
+python .\Driver.py $file"_output.nc"
+python .\tests\test_vals_v2.py $file"_output_processed.nc" $file".nc"
+
