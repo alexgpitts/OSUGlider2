f2fc2ff451c5f07a38a6f94cbb1443763fac6440
Alex Pitts <46640378+alexgpitts@users.noreply.github.com>
2022-05-12 19:27:06 -0700

    Create run3.ps1

---
+++ Python/scripts/run3.ps1
@@ -0,0 +1,5 @@
+# PI.ps1
+python .\csv_extractor.py --cdip .\ncFiles\067.20201225_1200.20201227_0400.nc
+python .\preprocessor.py --group=Meta --group=Wave --group=XYZ ".\ncFiles\067.20201225_1200.20201227_0400.nc"
+python .\Driver.py ".\ncFiles\067.20201225_1200.20201227_0400_output.nc"
+python .\tests\test_non_direction_v2.py --graph ".\ncFiles\067.20201225_1200.20201227_0400_output_processed.nc" ".\ncFiles\067.20201225_1200.20201227_0400.nc"
